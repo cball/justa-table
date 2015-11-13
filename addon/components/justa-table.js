1a8928aa608d3c650ba12d069d426691a8a22669
Chris Ball <chris@echobind.com>
2015-11-13 16:05:22 -0500

    - Add rowClasses to justa-table with comments - Add fake rowspan functionality (if true and column value is empty, adds class of ‘fake-rowspan’) - Add ability to pass row height and row classes to the top level table - Remove some cruft and unnecessary styles - Placeholder for colgroups

--- addon/components/justa-table.js
+++ addon/components/justa-table.js
@@ -7,9 +7,23 @@ export default Ember.Component.extend({
   layout,
   classNames: ['justa-table'],
 
+  /**
+    Css classes to apply to table rows.
+    @public
+  */
+  rowClasses: null,
+
+  /**
+    If content is empty.
+    @public
+  */
   noContent: empty('content'),
 
   actions: {
+    /**
+      Action triggered when the loader is in view.
+      @public
+    */
     infinityLoad() {
       this.sendAction('infinityLoad');
     }
---
+++ tests/integration/components/justa-table-test.js
@@ -0,0 +1,113 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('justa-table', 'Integration | Component | justa table', {
+  integration: true
+});
+
+function getCell(context, rowObject) {
+  let { row, cell } = rowObject;
+  return context.$(`tbody tr:nth-of-type(${row}) td:nth-of-type(${cell})`);
+}
+
+function getRow(context, rowObject) {
+  let { row } = rowObject;
+  return context.$(`tbody tr:nth-of-type(${row})`);
+}
+
+test('it renders content', function(assert) {
+  let content = [
+    { name: 'Fred' },
+    { name: 'Wilma' },
+    { name: undefined }
+  ];
+
+  this.set('content', content);
+
+  this.render(hbs`
+    {{#justa-table content=content as |table|}}
+      {{#table-columns table=table as |row|}}
+        {{table-column
+          row=row
+          headerName='foo'
+          valueBindingPath='name'}}
+
+      {{/table-columns}}
+    {{/justa-table}}
+  `);
+
+  assert.equal(this.$('th').text().trim(), 'foo', 'our column was named foo');
+  assert.equal(this.$('tr').length, 4, 'should have 2 rows and a header row');
+  assert.equal(getCell(this, { row: 1, cell: 1 }).text().trim(), 'Fred', 'first cell should be Fred');
+  assert.equal(getCell(this, { row: 2, cell: 1 }).text().trim(), 'Wilma', 'first cell should be Wilma');
+});
+
+test('adds a fake rowspan class if cell content isEmpty and useFakeRowspan is true', function(assert) {
+  let content = [
+    { name: 'Fred' },
+    { name: 'Wilma' },
+    { name: undefined }
+  ];
+
+  this.set('content', content);
+
+  this.render(hbs`
+    {{#justa-table content=content as |table|}}
+      {{#table-columns table=table as |row|}}
+        {{table-column
+          row=row
+          headerName='foo'
+          useFakeRowspan=true
+          valueBindingPath='name'}}
+
+      {{/table-columns}}
+    {{/justa-table}}
+  `);
+
+  assert.ok(getCell(this, { row: 3, cell: 1 }).hasClass('fake-rowspan'));
+});
+
+test('passes rowHeight to rows', function(assert) {
+  let content = [
+    { name: 'Fred' }
+  ];
+
+  this.set('content', content);
+
+  this.render(hbs`
+    {{#justa-table content=content as |table|}}
+      {{#table-columns table=table rowHeight='40px' as |row|}}
+        {{table-column
+          row=row
+          headerName='foo'
+          valueBindingPath='name'}}
+
+      {{/table-columns}}
+    {{/justa-table}}
+  `);
+
+  assert.equal(getRow(this, { row: 1 }).attr('style'), '40px', 'row height should be 40px');
+});
+
+test('adds rowClasses to rows', function(assert) {
+  let content = [
+    { name: 'Fred' }
+  ];
+
+  this.set('content', content);
+
+  this.render(hbs`
+    {{#justa-table content=content as |table|}}
+      {{#table-columns table=table rowClasses='hey man' as |row|}}
+        {{table-column
+          row=row
+          headerName='foo'
+          valueBindingPath='name'}}
+
+      {{/table-columns}}
+    {{/justa-table}}
+  `);
+
+  assert.ok(getRow(this, { row: 1 }).hasClass('hey'), 'row should have hey class');
+  assert.ok(getRow(this, { row: 1 }).hasClass('man'), 'row should have man class');
+});
