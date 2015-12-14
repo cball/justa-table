895dc687ef8d4fb2845e992f622c598ecef3caef
Chris Ball <chris@echobind.com>
2015-12-13 23:52:44 -0500

    Fixed column fixes, add textWrap, collapsable fixes
    - Add textWrap option to columns
    - Ensure headers are equal height when using fixed tables
    - Add hover class to rows by mouseenter index
    - Fix bug where collapsable rows needed 2 clicks to expand
    - Use Ember.set instead of = when setting rowGroup’s data

--- addon/components/justa-table.js
+++ addon/components/justa-table.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import layout from '../templates/components/justa-table';
 
 const { empty } = Ember.computed;
+const { run, isEmpty } = Ember;
 
 export default Ember.Component.extend({
   layout,
@@ -18,6 +19,32 @@ export default Ember.Component.extend({
     @public
   */
   noContent: empty('content'),
+
+  /**
+    Ensure header heights are equal. Schedules after render to ensure it's
+    called once per table.
+    @public
+  */
+  ensureEqualHeaderHeight() {
+    run.scheduleOnce('afterRender', this, this._ensureEqualHeaderHeight);
+  },
+
+  /**
+    If we have any fixed columns, make sure the fixed columns and standard
+    table column header heights stay in sync.
+    @private
+  */
+  _ensureEqualHeaderHeight() {
+    let fixedHeader = this.$('.fixed-table-columns th:first-of-type');
+    if (isEmpty(fixedHeader)) {
+      return;
+    }
+    let columnHeader = this.$('.table-columns th:first-of-type');
+    let maxHeight = Math.max(fixedHeader.height(), columnHeader.height());
+
+    fixedHeader.height(maxHeight);
+    columnHeader.height(maxHeight);
+  },
 
   actions: {
     /**
